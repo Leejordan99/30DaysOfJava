794731a3f91bed8a6c22c37addf903181826d8b6
Leejordan99 <Leejordan.robertson@Icloud.com>
2020-05-19 15:37:21 +0100

    my code

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Scanner in = new Scanner(System.in);
+        //String s = in.nextLine();
+
+
+
+        int i  = 0; //Initialization
+        do {
+            System.out.println("I is: " + i);
+
+        }
+        while(i <  10) { //Condition
+            System.out.println("i is: " + i);
+            i++;
+            
+    }
+    }
+
+
+
+    in.close();
+    }
+}
